92d0f104a0375e63bb47384fde1230e1eee897d7
AlejandroEspinozaT <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-11-14 08:31:47 -0400

    pub sub implementation with rabbit mq added

--- EcommerceP/EcommerceSender/sql/V3__Create_table_carts.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS db_schema.carts (
-   cart_id SERIAL PRIMARY KEY,
-   user_id INTEGER NOT NULL REFERENCES users(user_id),
-    product_id INTEGER NOT NULL REFERENCES products(id),
-    quantity INTEGER NOT NULL DEFAULT 1,
-    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
--- EcommerceP/EcommerceSender/sql/V4__Create_table_payments.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS db_schema.payments (
-    payment_id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES users(user_id),
-    cart_id INTEGER NOT NULL REFERENCES carts(cart_id),
-    amount DECIMAL(10, 2) NOT NULL,
-    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    payment_method VARCHAR(255),
-    payment_status VARCHAR(255) NOT NULL
-);
